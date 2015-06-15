3d989602478bceb0558d59ec59020e267aae5ba0
Yong Qian, Lee <b92101007@gmail.com>
2015-06-15 16:57:52 +0800

    利用instance紀錄狀態的範例

--- index.js
+++ index.js
@@ -2,20 +2,49 @@
 
 //Template只作用於使用者端，因此相關程式碼也應只在使用者端作用
 if (Meteor.isClient) {
+  //紀錄template clock instance的編號
+  var clockInstanceNumber = 0;
+  //當模版被建立的時候執行之
+  Template.clock.onCreated(function() {
+    var instance = this;
+    //每次有模版被創建時都將紀錄編號+1
+    clockInstanceNumber += 1;
+    //紀錄模板編號到instance上
+    instance.number = clockInstanceNumber;
+    //紀錄當前時間
+    instance.timer = new ReactiveVar( new Date() );
+    //定時更新當前時間
+    instance.interval = Meteor.setInterval(
+      function() {
+        instance.timer.set( new Date() );
+      }, 
+      1000
+    );
+  });
+  //當模版DOM在網頁上被建立時
+  Template.clock.onRendered(function() {
+    var instance = this;
+    //若模版編號是偶數，背景為綠色
+    if (instance.number % 2 === 0) {
+      instance.$('div').css('background', '#00FF00');
+    }
+    //否則為紅色
+    else {
+      instance.$('div').css('background', '#FF0000');
+    }
+  });
+  //當模版被移除時
+  Template.clock.onDestroyed(function() {
+    var instance = this;
+    //停止計時器
+    Meteor.clearInterval( instance.interval );
+  });
+
   //在template clock裡註冊名為nowString的helpers
   Template.clock.helpers({
     nowString: function() {
-      return Session.get('now').toLocaleString();
+      var instance = Template.instance();
+      return instance.timer.get().toLocaleString();
     }
   });
-
-  //設定now session的值為當前系統時間
-  Session.setDefault('now', new Date());
-  //每秒都將最新的系統時間更新到now session裡
-  Meteor.setInterval(
-    function() {
-      Session.set('now', new Date());
-    }, 
-    1000
-  );
 }
