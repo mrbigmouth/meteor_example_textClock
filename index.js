eae7acec1d4e749bff7484aa1988c6c6d87e78b1
Yong Qian, Lee <b92101007@gmail.com>
2015-06-17 17:00:13 +0800

    data context使用範例: #with block

--- index.js
+++ index.js
@@ -2,49 +2,49 @@
 
 //Template只作用於使用者端，因此相關程式碼也應只在使用者端作用
 if (Meteor.isClient) {
-  //紀錄template clock instance的編號
-  var clockInstanceNumber = 0;
-  //當模版被建立的時候執行之
-  Template.clock.onCreated(function() {
-    var instance = this;
-    //每次有模版被創建時都將紀錄編號+1
-    clockInstanceNumber += 1;
-    //紀錄模板編號到instance上
-    instance.number = clockInstanceNumber;
-    //紀錄當前時間
-    instance.timer = new ReactiveVar( new Date() );
-    //定時更新當前時間
-    instance.interval = Meteor.setInterval(
-      function() {
-        instance.timer.set( new Date() );
-      }, 
-      1000
-    );
+  //定義各時鐘的預設使用資料
+  var dataOfClock1 = new ReactiveVar({
+    timezone: 'Asia/Taipei',
+    timezoneAdjust: 28800000
   });
-  //當模版DOM在網頁上被建立時
-  Template.clock.onRendered(function() {
-    var instance = this;
-    //若模版編號是偶數，背景為綠色
-    if (instance.number % 2 === 0) {
-      instance.$('div').css('background', '#00FF00');
-    }
-    //否則為紅色
-    else {
-      instance.$('div').css('background', '#FF0000');
-    }
+  var dataOfClock2 = new ReactiveVar({
+    timezone: 'America/New_York',
+    timezoneAdjust: -14400000
   });
-  //當模版被移除時
-  Template.clock.onDestroyed(function() {
-    var instance = this;
-    //停止計時器
-    Meteor.clearInterval( instance.interval );
+  var dataOfClock3 = new ReactiveVar({
+    timezone: 'UTC',
+    timezoneAdjust: 0
   });
+
+  //定義body中#with使用的helpers
+  Template.registerHelper('dataOfClock1', function() {
+    return dataOfClock1.get();
+  });
+  Template.registerHelper('dataOfClock2', function() {
+    return dataOfClock2.get();
+  });
+  Template.registerHelper('dataOfClock3', function() {
+    return dataOfClock3.get();
+  });
+
+  //將「當前UTC時間」作為響應式資料來源
+  var currentUTCTime = new ReactiveVar( new Date( Date.now() + new Date().getTimezoneOffset() * 60 * 1000 ) );
+  //定時更新「當前UTC時間」
+  Meteor.setInterval(
+    function() {
+      currentUTCTime.set( new Date( Date.now() + new Date().getTimezoneOffset() * 60 * 1000 ) );
+    }, 
+    1000
+  );
 
   //在template clock裡註冊名為nowString的helpers
   Template.clock.helpers({
     nowString: function() {
-      var instance = Template.instance();
-      return instance.timer.get().toLocaleString();
+      //取得data context裡設定的時區修正值
+      var timezoneAdjust = this.timezoneAdjust;
+      //將utc時間加上時區修正值
+      var currentTimeInTimezone = currentUTCTime.get().getTime() + timezoneAdjust;
+      return new Date(currentTimeInTimezone).toLocaleString();
     }
   });
 }
